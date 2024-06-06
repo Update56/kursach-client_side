044ca12ea966ffe18f2c4372c533420b7cecd04c
Update56 <heupdate56@gmail.com>
2024-06-06 19:18:44 +0700

    Добавлена циклическое получение и отправка данных с сервера. Добавлена закрытие и синхоранизация горутин.

--- client.go
+++ client.go
@@ -4,7 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"sync"
 )
+
+// структура сообщения
+type Message struct {
+	receiver string
+	text     string
+}
 
 func main() {
 	fmt.Println("Hello, I'm client!")
@@ -31,8 +38,8 @@ func main() {
 	//объявляем поток вывода с источником в виде подключения
 	buf := bufio.NewReader(conn)
 
-	//считываем поток байтов пока не встретится перевод строки
 	line := make([]byte, 1024)
+	//считываем поток байтов
 	n, err := buf.Read(line)
 	//проверка на ошибку
 	if n == 0 || err != nil {
@@ -40,5 +47,60 @@ func main() {
 		return
 	}
 	fmt.Println(string(line[:n]))
+	//waitGroup для синхронизации горутин
+	var wg sync.WaitGroup
+	wg.Add(2)
+	//отправка сообщений
+	go sendingMessage(conn, &wg)
+	//получение сообщений
 
+	wg.Wait()
 }
+
+// функция отправки сообщений
+func sendingMessage(conn net.Conn, wg *sync.WaitGroup) {
+
+	quit := make(chan int)
+	go receivingMessage(conn, wg, quit)
+	for {
+		var text string
+		fmt.Scan(&text)
+		if text == "###" {
+			wg.Done()
+			close(quit)
+			break
+		}
+		conn.Write([]byte(text))
+	}
+}
+
+// функция получения сообщений
+func receivingMessage(conn net.Conn, wg *sync.WaitGroup, ch chan int) {
+	//объявляем поток вывода с источником в виде подключения
+	buf := bufio.NewReader(conn)
+	for {
+		select {
+		case <-ch:
+			wg.Done()
+			return
+		default:
+			line := make([]byte, 1024)
+			//считываем поток байтов
+			n, err := buf.Read(line)
+			//проверка на ошибку
+			if n == 0 || err != nil {
+				fmt.Println("Read error:", err)
+				wg.Done()
+				return
+			}
+			fmt.Println(string(line[:n]))
+		}
+	}
+}
+
+//функции "sendingMessage" и "receivingMessage" будут выполняться
+//в отдельных горутинах чтобы не блокировать ввод и вывод
+
+//проблема:нужно как-то выходить из диалога
+//2 решения: 1) топорное: ввод определённого набора символов (например: "###")
+//2) через UNIX-сигналы: при нажатии сочетания клавиш ctrl+c (^C) будет произведён выход из диалога в главное меню
