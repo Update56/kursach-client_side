e8734bb3f8c288b908ce616e6d4cd95fc8f0d431
Update56 <heupdate56@gmail.com>
2024-06-09 20:40:45 +0700

    Русификация, проверка на ввод никнейма, переписана функция получения сообщений

--- client.go
+++ client.go
@@ -2,27 +2,45 @@ package main
 
 import (
 	"bufio"
+	"encoding/gob"
 	"fmt"
 	"net"
+	"strings"
 	"sync"
 )
 
+// общая константа размера буффера сообщений
+const bufferMessSize byte = 64
+
+// зарезервированное имя клиента
+const clientName string = "client"
+
 // структура сообщения
 type Message struct {
-	receiver string
-	text     string
+	Sender   string //отправитель
+	Receiver string //получатель
+	Text     string //текст
 }
 
 func main() {
-	fmt.Println("Hello, I'm client!")
 
-	fmt.Println("Input you login")
+	//ввод никнейма
+	fmt.Println("Введите никнейм")
 	var nickname string
-	fmt.Scan(&nickname)
+	//проверка на имя пользователя, чтобы оно не совпадало с зарезервированным именем
+	for {
+		fmt.Scan(&nickname)
+		if strings.ToLower(nickname) != clientName {
+			break
+		}
+		fmt.Println("Это зарезервированное имя. Введите другое")
+	}
 
-	fmt.Println("Input server address")
+	//ввод IP-адреса:порт сервера
+	fmt.Println("Введите IP:port сервера")
 	var ipAddress string
 	fmt.Scan(&ipAddress)
+
 	//установка соединение с сервером по введённому <IP:port>
 	conn, err := net.Dial("tcp", ipAddress)
 	//проверка на ошибку при установке
@@ -33,7 +51,13 @@ func main() {
 	//закрытие в конце выполнения программы
 	defer conn.Close()
 
-	conn.Write([]byte(nickname))
+	//создание хэш-таблицы ключ - никнейм, значение - канал c единственной
+	var usersList = map[string]chan string{
+		clientName: make(chan string, bufferMessSize), //нулевая запись со спец.именем
+	}
+
+	go receivingMessage(conn, usersList)
+	//conn.Write([]byte(nickname))
 
 	//объявляем поток вывода с источником в виде подключения
 	buf := bufio.NewReader(conn)
@@ -53,48 +77,43 @@ func main() {
 	//отправка сообщений
 	go sendingMessage(conn, &wg)
 	//получение сообщений
-
 	wg.Wait()
 }
 
 // функция отправки сообщений
 func sendingMessage(conn net.Conn, wg *sync.WaitGroup) {
-
-	quit := make(chan int)
-	go receivingMessage(conn, wg, quit)
 	for {
 		var text string
 		fmt.Scan(&text)
 		if text == "###" {
 			wg.Done()
-			close(quit)
 			break
 		}
 		conn.Write([]byte(text))
 	}
 }
 
-// функция получения сообщений
-func receivingMessage(conn net.Conn, wg *sync.WaitGroup, ch chan int) {
-	//объявляем поток вывода с источником в виде подключения
-	buf := bufio.NewReader(conn)
+// функция получения сообщений и распределения
+func receivingMessage(conn net.Conn, usersList map[string]chan string) {
+
+	//объявляем входной декодер с источником в виде подключения
+	dec := gob.NewDecoder(conn)
+
+	//цикл получения и распределения сообщений
 	for {
-		select {
-		case <-ch:
-			wg.Done()
-			return
-		default:
-			line := make([]byte, 1024)
-			//считываем поток байтов
-			n, err := buf.Read(line)
-			//проверка на ошибку
-			if n == 0 || err != nil {
-				fmt.Println("Read error:", err)
-				wg.Done()
-				return
-			}
-			fmt.Println(string(line[:n]))
+		//пустая структура для сообщения
+		recMess := Message{}
+
+		//десериализация полученного сообщения
+		dec.Decode(&recMess)
+
+		//проверка на наличие пользователя в хэш-таблице
+		if _, available := usersList[recMess.Sender]; !available {
+			//если записи нет, создать
+			usersList[recMess.Sender] = make(chan string, bufferMessSize)
 		}
+		//отправляем сообщение в канал, соотвествующий никнейму отправителя
+		usersList[recMess.Sender] <- recMess.Text
 	}
 }
 
